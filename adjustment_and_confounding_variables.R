e287005bfa9a138499a8f051b82ead9631f52009
Amy Yang <yan.yang@student.uts.edu.au>
2021-10-17 00:51:30 +1100

    update the R script for Adjustment

--- adjustment_and_confounding_variables.R
+++ adjustment_and_confounding_variables.R
@@ -14,12 +14,12 @@ e_burnout <-clean_names(e_burnout)
 
 
 # View the data structure -------------------------------------------------
+
 names(e_burnout)
 
 glimpse(e_burnout)
 
 e_burnout$date_of_joining <- as.Date(e_burnout$date_of_joining,"%d/%m/%Y")
-
 
 
 # Check missing values ----------------------------------------------------
@@ -37,7 +37,6 @@ cbind(
 )
 
 # colSums(is.na(employee_burnout)) can also check missing values for columns
-
 
 
 # Transform the categorical variables and date variable -------------------
@@ -71,74 +70,69 @@ corrplot(corr_burnout, type = "upper", tl.col = "darkblue", tl.srt = 45,addCoef.
 
 lm_w <- lm(burn_rate ~ iswfh, data = e_burnout_num)
 summary(lm_w)
-coef(lm_w)
 
 e_burnout_wfh <- e_burnout_num %>% filter(iswfh==1)
 e_burnout_notwfh <- e_burnout_num %>% filter(iswfh==0)
 
 ggplot(e_burnout_num,aes(x=as.factor(iswfh),y=burn_rate))+
   geom_point(aes(color=wfh_setup_available))+
-  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "light blue", lwd = 1, lty = 2) +
+  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "aquamarine2", lwd = 1, lty = 2) +
   geom_hline(yintercept = mean(e_burnout_notwfh$burn_rate), color = "pink", lwd = 1, lty = 2)+
-  labs(x="Whether work from home facility is available", y="Employee burnout rate",color="WFH setup availability")
+  labs(x="Whether work from home facility is available", y="Employee burnout rate",color="WFH setup available")+
+  theme_bw()
 
 #mental_fatigue_score
 
 lm_m <- lm(burn_rate ~ mental_fatigue_score, data = e_burnout_num)
 summary(lm_m)
-coef(lm_m)
 
 lm_mw <- lm(burn_rate ~ mental_fatigue_score+iswfh, data = e_burnout_num)
 summary(lm_mw)
-coef(lm_mw)
-
 
 ggplot(e_burnout_num,aes(mental_fatigue_score,burn_rate))+
   geom_point(aes(color=wfh_setup_available))+
-  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "light blue", lwd = 1, lty = 2) +
+  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "aquamarine2", lwd = 1, lty = 2) +
   geom_hline(yintercept = mean(e_burnout_notwfh$burn_rate), color = "pink", lwd = 1, lty = 2)+
-  geom_abline(intercept = coef(lm_mw)[1]+coef(lm_mw)[3], slope = coef(lm_mw)[2], color = "light blue", lwd = 1.5) +
+  geom_abline(intercept = coef(lm_mw)[1]+coef(lm_mw)[3], slope = coef(lm_mw)[2], color = "aquamarine2", lwd = 1.5) +
   geom_abline(intercept = coef(lm_mw)[1], slope = coef(lm_mw)[2], color = "pink", lwd = 1.5) +
   geom_abline(intercept = coef(lm_m)[1], slope = coef(lm_m)[2])+
-  labs(x="Mental Fatigue Score",y="Employee burnout rate",color="WFH setup availability")
-
+  labs(x="Mental Fatigue Score",y="Employee burnout rate",color="WFH setup available")+
+  theme_bw()
 
 #resource_allocation
 
 lm_r <- lm(burn_rate ~ resource_allocation, data = e_burnout_num)
 summary(lm_r)
-coef(lm_r)
 
 lm_rw <- lm(burn_rate ~ resource_allocation+iswfh, data = e_burnout_num)
 summary(lm_rw)
-coef(lm_rw)
 
 ggplot(e_burnout_num,aes(resource_allocation,burn_rate))+
   geom_point(aes(color=wfh_setup_available))+
-  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "light blue", lwd = 1, lty = 2) +
+  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "aquamarine2", lwd = 1, lty = 2) +
   geom_hline(yintercept = mean(e_burnout_notwfh$burn_rate), color = "pink", lwd = 1, lty = 2)+
-  geom_abline(intercept = coef(lm_rw)[1]+coef(lm_rw)[3], slope = coef(lm_rw)[2], color = "light blue", lwd = 1.5) +
+  geom_abline(intercept = coef(lm_rw)[1]+coef(lm_rw)[3], slope = coef(lm_rw)[2], color = "aquamarine2", lwd = 1.5) +
   geom_abline(intercept = coef(lm_rw)[1], slope = coef(lm_rw)[2], color = "pink", lwd = 1.5) +
   geom_abline(intercept = coef(lm_r)[1], slope = coef(lm_r)[2])+
-  labs(x="Resource allocation",y="Employee burnout rate",color="WFH setup availability")
+  labs(x="Resource allocation",y="Employee burnout rate",color="WFH setup available")+
+  theme_bw()
 
 
 #designation
 
 lm_d <- lm(burn_rate ~ designation, data = e_burnout_num)
 summary(lm_d)
-coef(lm_d)
 
 lm_dw <- lm(burn_rate ~ designation+iswfh, data = e_burnout_num)
 summary(lm_dw)
-coef(lm_dw)
 
 ggplot(e_burnout_num,aes(designation,burn_rate))+
   geom_point(aes(color=wfh_setup_available))+
-  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "light blue", lwd = 1, lty = 2) +
+  geom_hline(yintercept = mean(e_burnout_wfh$burn_rate), color = "aquamarine2", lwd = 1, lty = 2) +
   geom_hline(yintercept = mean(e_burnout_notwfh$burn_rate), color = "pink", lwd = 1, lty = 2)+
-  geom_abline(intercept = coef(lm_dw)[1]+coef(lm_dw)[3], slope = coef(lm_dw)[2], color = "light blue", lwd = 1.5) +
+  geom_abline(intercept = coef(lm_dw)[1]+coef(lm_dw)[3], slope = coef(lm_dw)[2], color = "aquamarine2", lwd = 1.5) +
   geom_abline(intercept = coef(lm_dw)[1], slope = coef(lm_dw)[2], color = "pink", lwd = 1.5) +
   geom_abline(intercept = coef(lm_d)[1], slope = coef(lm_d)[2])+
-  labs(x="Designation",y="Employee burnout rate",color="WFH setup availability")
+  labs(x="Designation",y="Employee burnout rate",color="WFH setup available")+
+  theme_bw()
 
